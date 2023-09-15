833a4d42fc81fc897bad77e71e4d905dca5f9f8f
ArtemSh <elvis97rus@gmail.com>
2023-09-15 14:55:57 +0700

    cors конфиг

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         '/webhook/stripe',
-        '/api/orders/*'
+        '/api/orders/*',
+        '/api/*'
     ];
 }
